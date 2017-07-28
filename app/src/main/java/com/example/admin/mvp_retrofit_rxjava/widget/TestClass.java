3e1c43d3f98028b64deff05e78e2e7abc41cb8e4
admin <2754722688@qq.com>
2017-07-28 14:32:04 +0800

    add some code!

---
+++ app/src/main/java/com/example/admin/mvp_retrofit_rxjava/widget/TestClass.java
@@ -0,0 +1,13 @@
+package com.example.admin.mvp_retrofit_rxjava.widget;
+
+import android.util.Log;
+
+/**
+ * Created by admin on 2017/7/28.
+ */
+
+public class TestClass {
+    public TestClass() {
+        Log.e("cww", "haha");
+    }
+}
